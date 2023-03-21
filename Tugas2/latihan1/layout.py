20366c8f4d6568ace2c55624c434391ee595d7ac
CalvinOwen7 <102577926+CalvinOwen7@users.noreply.github.com>
2023-03-21 20:31:56 +0700

    Add files via upload

--- Tugas2/latihan1/layout.py
+++ Tugas2/latihan1/layout.py
@@ -7,7 +7,7 @@ st.header('Data Pendapatan')
 
 np.random.seed(200)
 df = pd.DataFrame({
-  'Bulan': ['Januari', 'Februari', 'Maret', 'April', 'Mei'],
+  'Bulan': ['1. Januari', '2. Februari', '3. Maret', '4. April', '5. Mei'],
   'Komputer': np.random.randint(100, 1000, 5),
   'Laptop': np.random.randint(100, 1000, 5),
   'Speaker': np.random.randint(100, 1000, 5)
